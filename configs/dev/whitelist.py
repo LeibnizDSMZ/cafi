3b5669c3e04e1a32595c98cc73a774100ef9cb2f
Artur Lissin <artdotlis@pm.me>
2025-09-25 22:13:12 +0000

    refactor: remove redundant key string check

---
+++ configs/dev/whitelist.py
@@ -0,0 +1,12 @@
+# type: ignore
+# ruff: noqa
+LATEST_VER  # unused variable (src/cafi/constants/versions.py:4)
+STABLE_VER  # unused variable (src/cafi/constants/versions.py:5)
+unk  # unused variable (src/cafi/container/acr_db.py:30)
+syn  # unused variable (src/cafi/container/acr_db.py:31)
+dep  # unused variable (src/cafi/container/acr_db.py:32)
+get_brc_merge_type  # unused function (src/cafi/container/fun/acr_db.py:13)
+create_catalogue_link  # unused function (src/cafi/library/catalogue.py:9)
+uuid_to_str  # unused function (src/cafi/container/fun/format.py:17)
+is_regex  # unused function (src/cafi/container/fun/format.py:23)
+create_ccno_links  # unused function (src/cafi/library/catalogue.py:25)
